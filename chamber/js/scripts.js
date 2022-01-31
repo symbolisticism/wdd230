57f797d6bd07a75efd401b5803126135106b5536
symbolisticism <peck.gabriele@gmail.com>
2022-01-30 21:02:43 -0700

    Added Chamber Home Page Build

---
+++ chamber/js/scripts.js
@@ -0,0 +1,25 @@
+/* script for hamburger menu */
+function toggleMenu() {
+  document.getElementById('primaryNav').classList.toggle('open');
+  document.getElementById('hamburgerBtn').classList.toggle('open');
+}
+
+const x = document.getElementById('hamburgerBtn');
+x.onclick = toggleMenu;
+
+/* dynamically updated copyright year */
+const d = new Date();
+const year = d.getFullYear();
+
+document.getElementById('copyrightyear').textContent = year;
+
+/* dynamically update the last modification */
+document.querySelector('#lastmod').textContent = `Last Updated: ${document.lastModified}`;
+
+/* dynamically updates the date in the header */
+const datefield = document.getElementById("date");
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	d
+);
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
