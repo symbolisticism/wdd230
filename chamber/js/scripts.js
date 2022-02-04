0d77782f1834f3c9c08fd27384cc4ae7f753c9da
symbolisticism <peck.gabriele@gmail.com>
2022-02-04 16:10:25 -0700

    Fixed layout

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -14,7 +14,7 @@ const year = d.getFullYear();
 document.getElementById('copyrightyear').textContent = year;
 
 /* dynamically update the last modification */
-document.querySelector('#lastmod').textContent = `Last Updated: ${document.lastModified}`;
+document.querySelector('#lastmod').textContent = `Last Modification: ${document.lastModified}`;
 
 /* dynamically updates the date in the header */
 const datefield = document.getElementById("date");
