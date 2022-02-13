713a235245e1a94770a3f94b14534459889708f9
symbolisticism <peck.gabriele@gmail.com>
2022-02-12 18:23:55 -0700

    calculated the windchill with JS

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,19 +1,17 @@
-function windchill() {
-  let temperature = parseFloat(document.querySelector(".temperature").value);
-  let windspeed = parseFloat(document.querySelector(".windspeed").value);
-  let windchill;
+let temperature = parseFloat(document.querySelector(".temperature").textContent); 
+let windspeed = parseFloat(document.querySelector(".windspeed").textContent);
+let windchill = '';
 
-  if (temperature <= 50 && windspeed > 3) {
-    windchill =
-      35.74 +
-      0.6215 * temperature -
-      35.75 * Math.pow(windspeed, 0.16) +
-      0.4275 * temperature * Math.pow(windspeed, 0.16);
-    
-    document.querySelector('.windchill').innerHTML = `${windchill}&#8457;`;
-    
-  } else {
-    windchill = 'N/A';
-    document.querySelector('.windchill').textContent = windchill;
-  }
+if (temperature <= 50 && windspeed > 3) {
+  windchill = windchill(temperature, windspeed);
+  windchill = `${windchill} &#176;F`
+} else {
+  windchill = 'N/A'; 
+}
+
+// output
+document.querySelector('.windchill').innerHTML = windchill;
+
+function windchill(temp, speed) {
+  return 35.74 + (0.6215 * temp) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * temp * Math.pow(speed, 0.16));
 }
