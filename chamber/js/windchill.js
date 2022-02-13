5b2064739665b34bb100b8222b9733fcf4b8e9e4
symbolisticism <peck.gabriele@gmail.com>
2022-02-12 21:23:25 -0700

    fixed windspeed JS

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,17 +1,10 @@
-let temperature = parseFloat(document.querySelector(".temperature").textContent); 
-let windspeed = parseFloat(document.querySelector(".windspeed").textContent);
-let windchill = '';
+const t = parseFloat(document.querySelector(".temperature").textContent);
+const w = parseFloat(document.querySelector(".windspeed").textContent);
 
-if (temperature <= 50 && windspeed > 3) {
-  windchill = windchill(temperature, windspeed);
-  windchill = `${windchill} &#176;F`
-} else {
-  windchill = 'N/A'; 
+if(t <= 50 && w > 3) {
+    const windchill = 35.74 + 0.6215 * t - 35.75 * Math.pow(w,0.16) + 0.4275 * t * Math.pow(w,0.16)
+    document.querySelector(".windchill").innerHTML = `${Math.round(windchill)}&#176;F`;
 }
-
-// output
-document.querySelector('.windchill').innerHTML = windchill;
-
-function windchill(temp, speed) {
-  return 35.74 + (0.6215 * temp) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * temp * Math.pow(speed, 0.16));
+else {
+    document.querySelector(".windchill").innerHTML = "N/A"
 }
