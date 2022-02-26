9ea60463aaa8b7daaf2af00a0e494f18a6c2fb6c
symbolisticism <peck.gabriele@gmail.com>
2022-02-26 09:28:58 -0700

    made hidden date-time element, fixed some css join

--- chamber/js/join.js
+++ chamber/js/join.js
@@ -1,11 +1,15 @@
-function showDate() {
+  
+  // let d = Date();
+  // let timeAtLoad = `${d.getMonth()} ${d.getDate()}, ${d.getFullYear()}`;
 
-  let d = new Date();
-  let timeAtLoad = `${d.getMonth()} ${d.getDate()}, ${d.getFullYear()}`;
+  // let time = document.querySelector('.time');
 
-  let time = document.querySelector('.time');
+  // document.querySelector('.time').textContent = timeAtLoad;
 
-  document.querySelector('.time').textContent = timeAtLoad;
+let timeAtLoad = new Date().toISOString();
 
+timeAtLoad = timeAtLoad.substring(0, 16);
 
-}
+document.querySelector('.time').textContent = timeAtLoad;
+
+console.log(timeAtLoad);
