c69d4926395b0c368c406c581249b7df290f80cb
symbolisticism <peck.gabriele@gmail.com>
2022-02-26 11:56:52 -0700

    fixed wayfinding, added thanks page for join page

--- chamber/js/join.js
+++ chamber/js/join.js
@@ -1,11 +1,3 @@
-  
-  // let d = Date();
-  // let timeAtLoad = `${d.getMonth()} ${d.getDate()}, ${d.getFullYear()}`;
-
-  // let time = document.querySelector('.time');
-
-  // document.querySelector('.time').textContent = timeAtLoad;
-
 let timeAtLoad = new Date().toISOString();
 
 timeAtLoad = timeAtLoad.substring(0, 16);
--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -29,9 +29,9 @@ window.onload = function() {
 
   const banner = document.getElementById('chamber-meeting')
 
-  if (d.getDay() == 1 || d.getDay() == 2)
-    banner.style.display = 'block';
-  else if (d.getDay() != 'Monday' && d.getDay() != 'Tuesday')
+  if (d.getDay() != 1 && d.getDay() != 2)
     banner.style.display = 'none';
+  else
+    banner.style.display = 'block';
   
 }
