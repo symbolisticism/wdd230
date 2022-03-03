da5597c06e2ddc164e8f8a881fee23c9175247f7
symbolisticism <peck.gabriele@gmail.com>
2022-03-03 11:40:49 -0700

    directory page js

--- chamber/js/businesses.js
+++ chamber/js/businesses.js
@@ -1,4 +1,4 @@
-const requestURL = 'https://symbolisticism.github.io/wdd230/chamber/js/businesses.js';
+const requestURL = 'https://symbolisticism.github.io/wdd230/chamber/js/data.json';
 const cards = document.querySelector('.cards');
 
 fetch(requestURL)
@@ -14,28 +14,34 @@ fetch(requestURL)
 function displayBusinesses(business) {
   // Create elements to add to the document
   let card = document.createElement('section');
-  let h2 = document.createElement('h2');
-  let portrait = document.createElement('img');
-  let birthdate = document.createElement('div');
-  let birthplace = document.createElement('div');
+  let picture = document.createElement('img');
+  let address = document.createElement('div');
+  let phone = document.createElement('div');
+  let url = document.createElement('div');
+
 
   // give the card a class
   card.setAttribute('class', 'cardclass');
 
-  birthdate.setAttribute('class', 'birth');
-  birthplace.setAttribute('class', 'birth');
-
-  // Change the textContent property of the h2 element to contain the prophet's full name
-  h2.textContent = `${business.name}`;
-
   // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
-  portrait.setAttribute('src', business.icon);
-  portrait.setAttribute('alt', `Picture of ${business.name}`);
-  portrait.setAttribute('loading', 'lazy');
+  picture.setAttribute('src', business.icon);
+  picture.setAttribute('alt', `Picture of ${business.name}`);
+  picture.setAttribute('loading', 'lazy');
 
-  // Add/append the section(card) with the h2 element
-  card.appendChild(h2);
-  card.appendChild(portrait);
+  // pouplate the address using the JSON file
+  address.textContent = `${business.address}`;
+
+  // pouplate the phone number using the JSON file
+  phone.textContent = `${business.phone}`;
+
+  // pouplate the url using the JSON file
+  url.textContent = `${business.url}`;
+
+  // Add/append the section(card) with all elements
+  card.appendChild(picture);
+  card.append(address);
+  card.append(phone);
+  card.append(url);
 
   // Add/append the existing HTML div with the cards class with the section(card)
   cards.appendChild(card);
